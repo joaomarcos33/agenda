3fdc81f20736024fc4addc7d4dbb088ebc59ebb5
joaomarcos33 <jm.marinhobaron@gmail.com>
2026-02-03 11:24:18 -0300

    agenda estilizada 1.0

--- app.py
+++ app.py
@@ -1,26 +1,24 @@
 from flask import Flask, render_template, request
-
 from models.tarefa import Tarefa
 
 app = Flask(__name__)
 
+# lista em memória (simples)
+tarefas = []
+
+
 @app.route('/')
 def home():
-    return render_template('home.html', titulo='home')
+    return render_template('home.html', titulo='Home')
 
 
-@app.route('/agenda', methods=['GET','POST'])
+@app.route('/agenda', methods=['GET', 'POST'])
 def agenda():
-    tarefa = None
-
     if request.method == 'POST':
-        titulo_tarefa = request.form['titulo-tarefa']
-        data_conclusao = request.form['data-conclusao']
+        titulo_tarefa = request.form.get('titulo-tarefa')
+        data_conclusao = request.form.get('data-conclusao')
+
         tarefa = Tarefa(titulo_tarefa, data_conclusao)
-        tarefa.salvar_tarefa()
+        tarefas.append(tarefa)
 
-    return render_template('agenda.html', titulo='Agenda', tarefa=tarefa)
-
-@app.route('/ola')
-def ola_mundo():
-    return "Olá, Mundo"
+    return render_template('agenda.html', titulo='Agenda', tarefas=tarefas)
--- models/tarefa.py
+++ models/tarefa.py
@@ -1,13 +1,4 @@
-from typing import Self
-
-from models.database import Database
-
 class Tarefa:
-    def __init__(self: Self, titulo_tarefa: str, data_conclusao =None)-> None:
-        self.titulo: str = titulo_tarefa
-        self.data_conclusao: str  = data_conclusao 
-    def salvar_tarefa(self: Self)-> None:
-        with Database('./data/tarefas.sqlite3') as db:
-            query: str = " INSERT INTO tarefas (titulo_tarefa, data_conclusao) VALUES (?, ?);"
-            params: tuple = (self.titulo, self.data_conclusao)
-            db.executar(query, params)    
+    def __init__(self, titulo_tarefa, data_conclusao):
+        self.titulo_tarefa = titulo_tarefa
+        self.data_conclusao = data_conclusao
